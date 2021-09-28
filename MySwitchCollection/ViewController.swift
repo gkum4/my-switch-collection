20c4929ac8c131d775288f283b05f5cb9a8f7e37
gkum4 <gustavokumasawa@gmail.com>
2021-09-28 09:43:04 -0300

    Initial Commit

---
+++ MySwitchCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySwitchCollection
+//
+//  Created by Gustavo Kumasawa on 28/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
